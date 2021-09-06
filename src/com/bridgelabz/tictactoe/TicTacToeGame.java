f4fdb316fd71cf769d0846df4f325feb95610dff
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-06 19:54:17 +0530

    ADDED: method to for user to make a move

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -3,6 +3,7 @@ package com.bridgelabz.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	static Scanner sc = new Scanner(System.in);
 	public static char[] creatBoard() {
 		char[] board = new char[10];
 		for(int i=0;i<10;i++) {
@@ -13,7 +14,7 @@ public class TicTacToeGame {
 	
 	
 	public static char selectLetter() {
-		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("select letter 1:X or 2:O");
 		int option = sc.nextInt();
 		if(option != 1 && option !=2) {
@@ -27,9 +28,21 @@ public class TicTacToeGame {
 	public static void showBoard(char[] board) {
 		System.out.println("Current status of board : ");
 		for(int i=1;i<10;i++) {
-			if(i%3 == 0) System.out.println();
 			System.out.print(board[i]);
+			if(i%3 == 0) {
+				System.out.println();
+				continue;
+			}
+			System.out.print(" | ");
+			
 		}
+	}
+	
+	public static char[] makeMove(char[] board, char playerLetter) {
+		System.out.println("Enter position(1-9) to make your move");
+		int position = sc.nextInt();
+		board[position] = playerLetter;
+		return board;
 	}
 	
 	public static void main(String[] args) {
@@ -38,6 +51,8 @@ public class TicTacToeGame {
 		char playerLetter = selectLetter();
 		char computerLetter = (playerLetter == 'X')?'O':'X';
 		System.out.println("Player letter : "+playerLetter+"  Computer letter : "+ computerLetter);
+		showBoard(board);
+		board = makeMove(board,playerLetter);
 		showBoard(board);
 	}
 }
