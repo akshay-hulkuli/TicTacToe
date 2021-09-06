2c2b8680d95d6214362857bdcfd2c4d9a760924b
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-06 09:22:12 +0530

    ADDED: method that prints the board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -24,11 +24,20 @@ public class TicTacToeGame {
 		
 	}
 	
+	public static void showBoard(char[] board) {
+		System.out.println("Current status of board : ");
+		for(int i=1;i<10;i++) {
+			if(i%3 == 0) System.out.println();
+			System.out.print(board[i]);
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to TIC-TAC-TOE game");
 		char[] board = creatBoard();
 		char playerLetter = selectLetter();
 		char computerLetter = (playerLetter == 'X')?'O':'X';
 		System.out.println("Player letter : "+playerLetter+"  Computer letter : "+ computerLetter);
+		showBoard(board);
 	}
 }
