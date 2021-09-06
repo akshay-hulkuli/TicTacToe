3a542ea7b83347346bca60f59c6d88a14d6168e7
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-06 08:22:43 +0530

    ADDED: method that initializes board

---
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToeGame {
+	private char[] board;
+	
+	public TicTacToeGame() {
+		board = new char[10];
+		
+	}
+	public void creatBoard() {
+		for(int i=0;i<10;i++) {
+			board[i] = 0;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to TIC-TAC-TOE game");
+		TicTacToeGame myGame = new TicTacToeGame();
+		myGame.creatBoard();
+	}
+}
